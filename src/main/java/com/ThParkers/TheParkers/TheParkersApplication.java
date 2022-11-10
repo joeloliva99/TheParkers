2ee91fed520e4fedd348bdd61bc8f8a64ed8ae3e
joeloliva99 <joeloliva99@gmail.com>
2022-11-10 17:40:59 -0300

    PrimerCommit

---
+++ src/main/java/com/ThParkers/TheParkers/TheParkersApplication.java
@@ -0,0 +1,13 @@
+package com.ThParkers.TheParkers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheParkersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheParkersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ThParkers/TheParkers/TheParkersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ThParkers.TheParkers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheParkersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
