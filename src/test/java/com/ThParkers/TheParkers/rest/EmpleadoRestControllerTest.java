5b9369bfd9ddf03fb4231a2f2fb511ed52d21188
MacarenaAlvear <macarena.alvear1901@alumnos.ubiobio.cl>
2022-11-27 22:00:59 -0300

    commit s2 1

---
+++ src/test/java/com/ThParkers/TheParkers/rest/EmpleadoRestControllerTest.java
@@ -0,0 +1,9 @@
+package com.ThParkers.TheParkers.rest;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class EmpleadoRestControllerTest {
+
+}
---
+++ src/test/java/com/ThParkers/TheParkers/service/EmpleadoServiceTest.java
@@ -0,0 +1,9 @@
+package com.ThParkers.TheParkers.service;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class EmpleadoServiceTest {
+
+}
