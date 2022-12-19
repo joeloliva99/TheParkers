34f9705f44f91f50b2a56a0d208f623fb92ad2dd
joeloliva99 <joeloliva99@gmail.com>
2022-12-19 03:37:44 -0300

    commitA

--- src/main/java/com/TheParkers/TheParkers/TheParkersApplication.java
+++ src/main/java/com/TheParkers/TheParkers/TheParkersApplication.java
@@ -1,4 +1,4 @@
-package com.ThParkers.TheParkers;
+package com.TheParkers.TheParkers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
